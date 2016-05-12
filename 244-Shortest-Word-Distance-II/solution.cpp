a4eb66bfd547578219d4a07ffff1be9a91012698
Yang Shi <kid7st@gmail.com>
2016-05-11 23:41:53 -0400

    [244. Shortest Word Distance II][Compile Error]

---
+++ 244-Shortest-Word-Distance-II/solution.cpp
@@ -0,0 +1,37 @@
+class WordDistance {
+public:
+    WordDistance(vector<string>& words) {
+        words_size = words.size();
+        for(int i = 0; i<words.size(); i++){
+            if(positionMap.count(words[i]) == 0){
+                vector<int> postions;
+                positions.push_back(i);
+                positionMap[words[i]] = positions;
+            }else{
+                positionMap[words[i]].push_back(i);
+            }
+        }
+    }
+
+    int shortest(string word1, string word2) {
+        int ret = 
+        for(int i = 0; i<positionMap[word1].size(); i++){
+            for(int j = 0; j<postionMap[word2].size(); j++){
+                int current = positionMap[word1][i] - positionMap[word2][j];
+                if(current < 0)
+                    current = -current;
+                if(current < ret)
+                    ret = current;
+            }
+        }
+    }
+private:
+    int words_size;
+    unordered_map<string, vector<int>> positionMap;
+};
+
+
+// Your WordDistance object will be instantiated and called as such:
+// WordDistance wordDistance(words);
+// wordDistance.shortest("word1", "word2");
+// wordDistance.shortest("anotherWord1", "anotherWord2");
