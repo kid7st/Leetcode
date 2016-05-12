280de8edaed2b79dc0bb04354e31b0eaa4daf7bd
Yang Shi <kid7st@gmail.com>
2016-05-11 23:49:43 -0400

    [244. Shortest Word Distance II][Accepted]

--- 244-Shortest-Word-Distance-II/solution.cpp
+++ 244-Shortest-Word-Distance-II/solution.cpp
@@ -3,13 +3,7 @@ public:
     WordDistance(vector<string>& words) {
         words_size = words.size();
         for(int i = 0; i<words.size(); i++){
-            if(positionMap.count(words[i]) == 0){
-                vector<int> positions;
-                positions.push_back(i);
-                positionMap[words[i]] = positions;
-            }else{
-                positionMap[words[i]].push_back(i);
-            }
+            positionMap[words[i]].push_back(i);
         }
     }
 
@@ -17,15 +11,13 @@ public:
         int ret = words_size;
         for(int i = 0; i<positionMap[word1].size(); i++){
             for(int j = 0; j<positionMap[word2].size(); j++){
-                int current = positionMap[word1][i] - positionMap[word2][j];
-                if(current < 0)
-                    current = -current;
-                if(current < ret)
-                    ret = current;
+                int current = abs(positionMap[word1][i] - positionMap[word2][j]);
+                ret = min(ret, current);
             }
         }
         return ret;
     }
+    
 private:
     int words_size;
     unordered_map<string, vector<int>> positionMap;
