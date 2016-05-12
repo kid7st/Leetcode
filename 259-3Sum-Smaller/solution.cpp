bb1f3d8fd8064c848096e8bfa0bb8322219de638
Yang Shi <kid7st@gmail.com>
2016-05-12 00:28:08 -0400

    [259. 3Sum Smaller][Accepted]

--- 259-3Sum-Smaller/solution.cpp
+++ 259-3Sum-Smaller/solution.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
+        if(nums.size() < 3)
+            return 0;
+        sort(nums.begin(), nums.end());
+        
         int count = 0;
         for(int i = 0; i<nums.size() - 2; i++){
             int k = nums.size() - 1;
-            for(int j = i + 1; j < nums.size() - 1; j++){
+            for(int j = i + 1; j < nums.size() - 1 && k > j; j++){
                 while(k > j && nums[i] + nums[j] + nums[k] >= target)
                     k--;
+                    
                 count += k - j;
                 if(k == j)
-                    continue;
+                    break;
             }
         }
+        
         return count;
     }
 };
