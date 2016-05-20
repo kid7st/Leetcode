e127692bde301029fb6c956acbfbace74307df64
Yang Shi <kid7st@gmail.com>
2016-05-20 15:15:03 -0400

    [163. Missing Ranges][Accepted]

--- 163-Missing-Ranges/solution.cpp
+++ 163-Missing-Ranges/solution.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
         vector<string> res;
+        if(nums.size() == 0){
+            if(lower == upper){
+                res.push_back(to_string(upper));
+            }else if(lower < upper){
+                res.push_back(to_string(lower) + "->" + to_string(upper));
+            }
+            
+            return res;
+        }
         
-        if(lower == nums[0] - 2){
+        if(lower == nums[0] - 1){
             res.push_back(to_string(nums[0] - 1));
-        }else if(lower < nums[0] - 2){
-            res.push_back(to_string(lower + 1) + "->" + to_string(nums[0] - 1));
+        }else if(lower < nums[0] - 1){
+            res.push_back(to_string(lower) + "->" + to_string(nums[0] - 1));
         }
         
         for(int i = 1; i < nums.size(); i++){
@@ -18,10 +27,10 @@ public:
         }
         
         int n = nums.size() - 1;
-        if(nums[n] == upper - 2){
-            res.push_back(to_string(upper - 1));
-        }else if(nums[n] < upper - 2){
-            res.push_back(to_string(nums[n] + 1) + "->" + to_string(upper - 1));
+        if(nums[n] == upper - 1){
+            res.push_back(to_string(upper));
+        }else if(nums[n] < upper - 1){
+            res.push_back(to_string(nums[n] + 1) + "->" + to_string(upper));
         }
         
         return res;
