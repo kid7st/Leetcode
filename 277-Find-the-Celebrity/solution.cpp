43e58ed2f85891e836830d369003c7c3bba704d9
Yang Shi <kid7st@gmail.com>
2016-05-20 14:15:24 -0400

    [277. Find the Celebrity][Compile Error]

---
+++ 277-Find-the-Celebrity/solution.cpp
@@ -0,0 +1,26 @@
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        vector<bool> isCelebrity = vector<bool>(n, true);
+        vector<int> count = vectro<int>(n, 0);
+        
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                if(knows(i, j)){
+                    isCelebrity[i] = false;
+                    count[j]++;
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(isCelebrity[i] && count[i] == n - 1)
+                return i;
+        }
+        
+        return -1;
+    }
+};
