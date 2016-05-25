0b62ce0d3b5b0dc325826754140fe0b5bfa7b2a3
Yang Shi <kid7st@gmail.com>
2016-05-25 11:17:21 -0400

    [94. Binary Tree Inorder Traversal][Accepted]

--- 94-Binary-Tree-Inorder-Traversal/solution.cpp
+++ 94-Binary-Tree-Inorder-Traversal/solution.cpp
@@ -10,22 +10,20 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*> nodeStack;
-        
         vector<int> res;
-        nodeStack.push(root);
-        while(!nodeStack.empty()){
-            TreeNode* node = nodeStack.top();
-            nodeStack.pop();
-            res.push_back(node->val);
-            
-            if(node->right != NULL)
-                nodeStack.push(node->right);
-                
-            if(node->left != NULL)
-                nodeStack.push(node->left);
-        }
+        inorderTraversal(root, res);
         
         return res;
+    }
+    
+    void inorderTraversal(TreeNode* node, vector<int>& acc){
+        if(node == NULL)
+            return;
+        
+        inorderTraversal(node->left, acc);
+        acc.push_back(node->val);
+        inorderTraversal(node->right, acc);
+        
+        return;
     }
 };
