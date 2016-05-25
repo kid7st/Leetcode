035409b58f963fee4a49f3ee908f78de18fb0acf
Yang Shi <kid7st@gmail.com>
2016-05-24 23:40:30 -0400

    [170. Two Sum III - Data structure design][Time Limit Exceeded]

---
+++ 170-Two-Sum-III---Data-structure-design/solution.cpp
@@ -0,0 +1,28 @@
+class TwoSum {
+public:
+
+    // Add the number to an internal data structure.
+	void add(int number) {
+	    mp[number] += 1;
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	bool find(int value) {
+	    for(auto pair : mp){
+	        int num = pair.second;
+	        if(mp.find(value - num) != mp.end())
+	            return true;
+	    }
+	    
+	    return false;
+	}
+	
+private:
+    unordered_map<int, int> mp;
+};
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum;
+// twoSum.add(number);
+// twoSum.find(value);
