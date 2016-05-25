7b4aad209dc7176e3c7b9046e04f2dfa21a88680
Yang Shi <kid7st@gmail.com>
2016-05-25 00:45:29 -0400

    [242. Valid Anagram][Accepted]

--- 242-Valid-Anagram/solution.cpp
+++ 242-Valid-Anagram/solution.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        vector<int> mpS(128, 0);
+        vector<int> mpT(128, 0);
         
-        if(s == t)
-            return true;
-        else
-            return false;
+        for(char ch : s)
+            mpS[ch] += 1;
+        for(char ch : t)
+            mpT[ch] += 1;
+        
+        for(int i = 0; i < 128; i++){
+            if(mpS[i] != mpT[i])
+                return false;
+        }
+        
+        return true;
     }
 };
