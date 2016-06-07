7a5429062bf6eb9249be3c763b44a9efc2e861f1
Yang Shi <kid7st@gmail.com>
2016-06-07 16:12:53 -0400

    [222. Count Complete Tree Nodes][Accepted] Recursive Solution O(logN)

---
+++ 222-Count-Complete-Tree-Nodes/solutionRecursive.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ /* Recursive Solution */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int leftHeight = treeHeight(root->left);
+        int rightHeight = treeHeight(root->right);
+        
+        if(leftHeight > rightHeight){
+            return countNodes(root->left) + pow(2, rightHeight);
+        }else{
+            return pow(2, leftHeight) + countNodes(root->right);
+        }
+        
+        return 0;
+    }
+
+private:
+    int treeHeight(TreeNode* root){
+        int count = 0;
+        while(root != NULL){
+            count += 1;
+            root = root->left;
+        }
+        
+        return count;
+    }
+};
