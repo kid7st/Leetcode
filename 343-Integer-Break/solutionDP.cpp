272c526421eaed4963755e7d849ad48365cea6ee
Yang Shi <kid7st@gmail.com>
2016-06-10 10:56:39 -0400

    [343. Integer Break][Accepted] DP

---
+++ 343-Integer-Break/solutionDP.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1, 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i/2; j++){
+                dp[i] = max(dp[i], 
+                            max(j, dp[j]) * max(i - j, dp[i - j]));
+            }
+        }
+        
+        return dp[n];
+    }
+};
