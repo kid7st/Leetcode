263f7eb145ee8ab5de330c35b3ae001a2965f280
Yang Shi <kid7st@gmail.com>
2016-06-11 13:09:37 -0400

    [309. Best Time to Buy and Sell Stock with Cooldown][Accepted]

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
@@ -3,19 +3,21 @@ public:
     int maxProfit(vector<int>& prices) {
         if(prices.empty())
             return 0;
-        vector<int> maxSell(prices.size());
-        vector<int> maxBuy(prices.size());
-        
-        maxSell[0] = 0;
-        maxBuy[0] = 0 - prices[0];
-        for(int i = 1; i < prices.size(); i++){
-            int buyProfit = (i < 2 ? 0 : maxSell[i - 2]) - prices[i];
-            maxBuy[i] = max(maxBuy[i - 1], buyProfit);
             
-            int sellProfit = maxBuy[i - 1] + prices[i];
-            maxSell[i] = max(maxSell[i - 1], sellProfit);
+        int beforeMaxSell = 0;
+        int maxSell = 0;
+        int maxBuy = 0 - prices[0];
+        
+        for(int i = 1; i < prices.size(); i++){
+            int bpMaxBuy = maxBuy;
+            int buyProfit = (i < 2 ? 0 : beforeMaxSell) - prices[i];
+            maxBuy = max(maxBuy, buyProfit);
+            
+            int sellProfit = bpMaxBuy + prices[i];
+            beforeMaxSell = maxSell;
+            maxSell = max(maxSell, sellProfit);
         }
         
-        return maxSell[prices.size() - 1];
+        return maxSell;
     }
 };
