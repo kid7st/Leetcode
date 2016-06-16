c4da7e9e6bd33c3e8a2f1f2d3f2c49b4f1c05c6a
Yang Shi <kid7st@gmail.com>
2016-06-15 23:20:13 -0400

    [294. Flip Game II][Accepted]

--- 294-Flip-Game-II/solution.cpp
+++ 294-Flip-Game-II/solution.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool canWin(string s) {
-        if(s.size() < 2) return false;
-        
-        for(int i = 0; i < s.size() - 1; i++){
-            if(s[i] == '+' && s[i + 1] == '+'){
-                s[i] = '-'; s[i + 1] = '-';
+        for(int i = 1; i < s.size(); i++){
+            if(s[i - 1] == '+' && s[i] == '+'){
+                s[i - 1] = '-'; s[i] = '-';
                 if(!canWin(s)) return true;
-                s[i] = '+'; s[i + 1] = '+';
+                s[i - 1] = '+'; s[i] = '+';
             }
         }
         return false;
