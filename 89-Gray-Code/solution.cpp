16216b6744aea016222ca43422cb554dbb91fc1b
Yang Shi <kid7st@gmail.com>
2016-06-19 00:40:56 -0400

    [89. Gray Code][Accepted]

--- 89-Gray-Code/solution.cpp
+++ 89-Gray-Code/solution.cpp
@@ -5,13 +5,6 @@ public:
             return vector<int>(1, 0);
         }
         
-        if(n == 1){
-            vector<int> gray;
-            gray.push_back(0);
-            gray.push_back(1);
-            return gray;
-        }
-        
         vector<int> gray = grayCode(n - 1);
         int size = gray.size();
         int padding = 1 << (n - 1);
