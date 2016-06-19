1e4b7dc15fd503def277a7b027f89b92683824c7
Yang Shi <kid7st@gmail.com>
2016-06-19 00:48:21 -0400

    [77. Combinations][Accepted]

---
+++ 77-Combinations/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> current;
+        dfs(n, k, 1, current, res);
+        return res;
+    }
+    
+private:
+    void dfs(int n, int k, int start, vector<int> &current, vector<vector<int>> &acc){
+        if(current.size() == k){
+            acc.push_back(current);
+            return;
+        }
+        
+        for(int i = start; i <= n - (k - current.size() - 1); i++){
+            current.push_back(i);
+            dfs(n, k, i + 1, current, acc);
+            current.pop_back();
+        }
+        
+        return;
+    }
+};
