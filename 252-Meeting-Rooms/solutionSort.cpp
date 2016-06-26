ec2b29cef53acd2216d96ff71299d631ce77b381
Yang Shi <kid7st@gmail.com>
2016-06-26 10:41:41 -0400

    [252. Meeting Rooms][Accepted] Sort

--- 252-Meeting-Rooms/solutionSort.cpp
+++ 252-Meeting-Rooms/solutionSort.cpp
@@ -15,7 +15,9 @@ bool comp(Interval a, Interval b){
 class Solution {
 public:
     bool canAttendMeetings(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), comp);            
+        sort(intervals.begin(), intervals.end(), 
+            [](const Interval &a, const Interval &b){ return a.start < b.start; }); 
+            
         for(int i = 1; i < intervals.size(); i++){
             if(intervals[i - 1].end > intervals[i].start)
                 return false;
