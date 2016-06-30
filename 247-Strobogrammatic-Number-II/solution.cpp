3440fdbe749965cdcd4fab3f796d4776074b651d
Yang Shi <kid7st@gmail.com>
2016-06-30 08:12:12 -0400

    [247. Strobogrammatic Number II][Accepted]

--- 247-Strobogrammatic-Number-II/solution.cpp
+++ 247-Strobogrammatic-Number-II/solution.cpp
@@ -3,7 +3,6 @@ public:
     vector<string> findStrobogrammatic(int n) {
         if(n == 0) return {""};
         if(n == 1) return {"0", "1", "8"};
-        if(n == 2) return {"11", "69", "88", "96"};
         
         return findStrobogrammatic(n, n);
     }
@@ -11,7 +10,6 @@ public:
     vector<string> findStrobogrammatic(int n, int target){
         if(n == 0) return {""};
         if(n == 1) return {"0", "1", "8"};
-        if(n == 2) return {"00", "11", "69", "88", "96"};
         vector<string> res;
         vector<string> next2 = findStrobogrammatic(n - 2, target);
         
