34ae695eea82a95eabb58f9c4363dfb83fc83d84
Yang Shi <kid7st@gmail.com>
2016-06-30 23:13:52 -0400

    [211. Add and Search Word - Data structure design][Accepted]

---
+++ 211-Add-and-Search-Word---Data-structure-design/solution.cpp
@@ -0,0 +1,69 @@
+class TrieNode {
+public:
+    // Initialize your data structure here.
+    TrieNode() {
+        isKey = false;
+        //count = 0;
+        for(int i = 0; i < 26; i++)
+            children[i] = NULL;
+    }
+    
+    bool isKey;
+    //int count;
+    TrieNode* children[26];
+};
+
+class WordDictionary {
+public:
+
+    WordDictionary(){
+        root = new TrieNode();
+    }
+
+    // Adds a word into the data structure.
+    void addWord(string word) {
+        TrieNode *node = root;
+        for(char ch : word){
+            if(node->children[ch - 'a'] == NULL){
+                node->children[ch - 'a'] = new TrieNode();
+            }
+            node = node->children[ch - 'a'];
+        }
+        node->isKey = true;        
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    bool search(string word) {
+        return search(word, root);
+    }
+    
+    bool search(string word, TrieNode *root){
+        if(root == NULL) 
+            return false;
+        if(word.size() == 0)
+            return root->isKey;
+        if(word[0] == '.'){
+            bool found = false;
+            for(int i = 0; i < 25; i++){
+                found = found | search(word.substr(1), root->children[i]);
+                if(found) return true;
+            }
+        }else{
+            if(root->children[word[0] - 'a'] == NULL)
+                return false;
+            else
+                return search(word.substr(1), root->children[word[0] - 'a']);
+        }
+        
+        return false;
+    }
+    
+private:
+    TrieNode* root;
+};
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary;
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
