cdc5f8f0088cf3fdbed4339a874e893c0a7f848e
Yang Shi <kid7st@gmail.com>
2016-07-01 11:56:15 -0400

    [133. Clone Graph][Accepted]

--- 133-Clone-Graph/solution.cpp
+++ 133-Clone-Graph/solution.cpp
@@ -11,36 +11,23 @@ public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
         if(!node) return NULL;
         
-        unordered_set<int> visited;
-        queue<UndirectedGraphNode *> copyQueue;
         queue<UndirectedGraphNode *> oriQueue;
         unordered_map<int, UndirectedGraphNode *> nodeMap;
         oriQueue.push(node);
         UndirectedGraphNode *res = new UndirectedGraphNode(node->label);
-        nodeMap.insert(pair<int, UndirectedGraphNode *>(res->label, res));
-        copyQueue.push(res);
+        nodeMap[res->label] = res;
         while(!oriQueue.empty()){
             UndirectedGraphNode *ori = oriQueue.front();
             oriQueue.pop();
-            UndirectedGraphNode *copy = copyQueue.front();
-            copyQueue.pop();
-            
-            if(visited.find(ori->label) == visited.end()){
-                visited.insert(ori->label);
-            
-                for(auto next : ori->neighbors){
+            UndirectedGraphNode *copy = nodeMap[ori->label];
+            for(auto next : ori->neighbors){
+                if(nodeMap.find(next->label) == nodeMap.end()){
+                    nodeMap[next->label] = new UndirectedGraphNode(next->label);
                     oriQueue.push(next);
-                    if(nodeMap.find(next->label) == nodeMap.end()){
-                        copy->neighbors.push_back(new UndirectedGraphNode(next->label));
-                        nodeMap.insert(pair<int, UndirectedGraphNode *>(next->label, copy->neighbors.back()));
-                    }else{
-                        copy->neighbors.push_back(nodeMap[next->label]);
-                    }
-                    copyQueue.push(copy->neighbors.back());
                 }
+                copy->neighbors.push_back(nodeMap[next->label]);
             }
         }
-        
         return res;
     }
 };
