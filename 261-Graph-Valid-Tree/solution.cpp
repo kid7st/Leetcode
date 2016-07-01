0435697380a82dd25616f8102971c9e432cd1593
Yang Shi <kid7st@gmail.com>
2016-07-01 13:50:09 -0400

    [261. Graph Valid Tree][Accepted]

---
+++ 261-Graph-Valid-Tree/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool validTree(int n, vector<pair<int, int>>& edges) {
+        for(auto edge : edges){
+            int root1 = unionFind(edge.first);
+            int root2 = unionFind(edge.second);
+            if(root1 == root2){
+                return false;
+            }else{
+                unionMerge(root1, root2);          
+            }
+        }
+        for(int i = 1; i < n; i++)
+            if(unionFind(i - 1) != unionFind(i)) return false;
+        
+        return true;
+    }
+    
+private:
+    unordered_map<int, int> setRoot;
+    int unionFind(int node){
+        int root = node;
+        while(setRoot.find(root) != setRoot.end() && setRoot[root] != root)
+            root = setRoot[root];
+        setRoot[node] = root;
+        return root;
+    }
+    int unionMerge(int node1, int node2){
+        setRoot[unionFind(node2)] = unionFind(node1);
+        return setRoot[node1];
+    }
+};
